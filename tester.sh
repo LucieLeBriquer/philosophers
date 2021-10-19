008f3bd0eb96043a0f4a175076bcbb391fe55288
lle-briq <lle-briq@student.42.fr>
2021-10-19 19:30:57 +0200

    hop

--- tester.sh
+++ tester.sh
@@ -6,13 +6,13 @@
 #    By: lle-briq <lle-briq@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/10/03 17:52:18 by lle-briq          #+#    #+#              #
-#    Updated: 2021/10/03 17:52:19 by lle-briq         ###   ########.fr        #
+#    Updated: 2021/10/19 19:30:45 by lle-briq         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
 
-PHILO_PATH=./philo
+PHILO_PATH='./philo'
 
 ## colors
 
@@ -24,19 +24,9 @@ PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 NC='\033[0m'
 
-UNAME=$(uname -s)
-if [ $UNAME == "Darwin" ]
-then
-	TIMEOUT="gtimeout"
-else
-	TIMEOUT="timeout"
-fi
-
-## help
-
 function display_help()
 {
-	printf "usage:\t./philo nb_of_tests\n"
+	printf "usage:\t./tester.sh nb_of_tests\n"
 	exit
 }
 
@@ -62,7 +52,7 @@ function test1()
     for i in `seq 1 $1`;
     do
 		printf "\rTesting $PHILO_PATH 1 800 200 200\t\t%4d" $i
-		res1=$($TIMEOUT 5s $PHILO_PATH 1 800 200 200 | grep "die" | wc -l)
+		res1=$(timeout 5s $PHILO_PATH 1 800 200 200 | grep "die" | wc -l)
 		if [ "$res1" -ne "1" ]
 		then
 			error=$(($error + 1))
@@ -83,7 +73,7 @@ function test2()
     for i in `seq 1 $1`;
     do
 		printf "\rTesting $PHILO_PATH 5 800 200 200\t\t%4d" $i
-		res2=$($TIMEOUT 5s $PHILO_PATH 5 800 200 200 | grep "die" | wc -l)
+		res2=$(timeout 5s $PHILO_PATH 5 800 200 200 | grep "die" | wc -l)
 		if [ "$res2" -ne "0" ]
 		then
 			error=$(($error + 1))
@@ -104,7 +94,7 @@ function test3()
     for i in `seq 1 $1`;
     do
 		printf "\rTesting $PHILO_PATH 5 800 200 200 7\t\t%4d" $i
-		res3=$($TIMEOUT 5s $PHILO_PATH 5 800 200 200 7 | grep "enough" | wc -l)
+		res3=$(timeout 5s $PHILO_PATH 5 800 200 200 7 | grep "enough" | wc -l)
 		if [ "$res3" -ne "1" ]
 		then
 			error=$(($error + 1))
@@ -125,7 +115,7 @@ function test4()
     for i in `seq 1 $1`;
     do
 		printf "\rTesting $PHILO_PATH 4 410 200 200\t\t%4d" $i
-		res4=$($TIMEOUT 5s $PHILO_PATH 4 410 200 200 | grep "die" | wc -l)
+		res4=$(timeout 5s $PHILO_PATH 4 410 200 200 | grep "die" | wc -l)
 		if [ "$res4" -ne "0" ]
 		then
 			error=$(($error + 1))
@@ -146,7 +136,7 @@ function test5()
     for i in `seq 1 $1`;
     do
 		printf "\rTesting $PHILO_PATH 4 310 200 100\t\t%4d" $i
-		res5=$($TIMEOUT 5s $PHILO_PATH 4 310 200 100 | grep "die" | wc -l)
+		res5=$(timeout 5s $PHILO_PATH 4 310 200 100 | grep "die" | wc -l)
 		if [ "$res5" -ne "1" ]
 		then
 			error=$(($error + 1))
